852db35a2c879ecf83a9e50c4dd271c72f8b03f4
techlm77 <dominicyoungjackson@gmail.com>
2023-03-04 16:05:17 +0000

    Update Admin Launch.bat
    It's still not finish quite yet, there is things I need to finish off.

--- Admin Launch.bat
+++ Admin Launch.bat
@@ -25,6 +25,7 @@ cls
 cd ".\Room %room_number%\"
 
 :username
+echo %username% >> ".\userlist.txt"
 start cmd /c "..\Room %room_number%\DisplayRoom.bat"
 title Chatting as %username%
 echo. >>"..\Room %room_number%\chat.txt"
@@ -38,6 +39,9 @@ if /I "%input%" == "/clear" goto clear
 if /I "%input%" == "/change" goto change_room
 if /I "%input%" == "/help" goto help
 if /I "%input%" == "/quit" goto quit
+if /I "%input:~0,5%" == "/kick" goto kick_user
+if /I "%input:~0,4%" == "/ban" goto ban_user
+if /I "%input:~0,3%" == "/op" goto op_user
 if "%input%" == "" goto read_messages_room
 echo %time% - %username%: %input% >>"..\Room %room_number%\chat.txt"
 set input=
@@ -45,45 +49,116 @@ goto read_messages_room
 
 :help
 cls
-mode con: cols=54 lines=8
+mode con: cols=54 lines=11
 echo 1. /help 
 echo 2. /quit 
 echo 3. /change 
 echo 4. /clear (admin only)
-echo 5. /kick [username] (coming soon) 
-echo 6. /ban [username] (coming soon) 
-echo 7. /op [username] (coming soon) 
+echo 5. /kick [username] 
+echo 6. /ban [username] 
+echo 7. /op [username] 
+echo.
+echo [Note]: /ban and /op commands require admin access.
 pause
 goto read_messages_room
+
+:kick_user
+set user_to_kick=%input:~6%
+findstr /i /m /c:"%user_to_kick%" ".\userlist.txt" >nul 2>&1
+if %errorlevel%==0 (
+    for /f "delims=" %%i in ('type ".\userlist.txt" ^| find /v "%user_to_kick%"') do echo %%i>>".\userlist_new.txt"
+    move /y ".\userlist_new.txt" ".\userlist.txt" >nul 2>&1
+    echo %time% - %user_to_kick% has been kicked by %username%. >>"..\Room %room_number%\chat.txt"
+    echo You have kicked %user_to_kick% from the room.
+    goto read_messages_room
+) else (
+    echo User not found in the room.
+    goto read_messages_room
+)
+
+:ban_user
+findstr /i /m /c:"%username%" ".\userlist.txt" >nul 2>&1
+if %errorlevel%==0 (
+    set user_to_ban=%input:~5%
+    findstr /i /m /c:"%user_to_ban%" ".\userlist.txt" >nul 2>&1
+    if %errorlevel%==0 (
+        echo %time% - %user_to_ban% has been banned by %username%. >>"..\Room %room_number%\chat.txt"
+        echo You have banned %user_to_ban% from the room.
+        echo %user_to_ban% >>".\banned_users.txt"
+        goto read_messages_room
+    ) else (
+        echo User not found in the room.
+        goto read_messages_room
+    )
+)
+
+:op_user
+findstr /i /m /c:"%username%" ".\userlist.txt" >nul 2>&1
+if %errorlevel%==0 (
+set user_to_op=%input:~4%
+findstr /i /m /c:"%user_to_op%" ".\userlist.txt" >nul 2>&1
+if %errorlevel%==0 (
+echo %time% - %user_to_op% has been promoted to operator by %username%. >>"..\Room %room_number%\chat.txt"
+echo You have promoted %user_to_op% to operator.
+echo %user_to_op% >>".\operators.txt"
+goto read_messages_room
+) else (
+echo User not found in the room.
+goto read_messages_room
+)
+) else (
+echo You do not have admin access.
+goto read_messages_room
+)
+
+:quit
+echo %time% - %username% has left the room. >>"..\Room %room_number%\chat.txt"
+exit
+
 :clear
+findstr /v "^" ".\chat.txt" >".\chat_backup.txt"
 del ".\chat.txt"
-if not exist ".\chat.txt" goto create_chat
+echo %time% - %username% has cleared the chat history. >>"..\Room %room_number%\chat.txt"
 goto read_messages_room
 
 :change_room
-echo Please close the display chat.
-pause
+echo %time% - %username% has left the room. >>"..\Room %room_number%\chat.txt"
 cd ..
-mode con: cols=63 lines=25
 goto mainMenu
-
-:create_chat
-echo        __                     __   ________          __  >>.\chat.txt
-echo       / /   ____  _________ _/ /  / ____/ /_  ____ _/ /_ >>.\chat.txt
-echo      / /   / __ \/ ___/ __ `/ /  / /   / __ \/ __ `/ __/ >>.\chat.txt
-echo     / /___/ /_/ / /__/ /_/ / /  / /___/ / / / /_/ / /_   >>.\chat.txt
-echo    /_____/\____/\___/\__,_/_/   \____/_/ /_/\__,_/\__/   >>.\chat.txt
-echo          LocalChat developed by Dominic J Young >>.\chat.txt
-echo.  >>.\chat.txt
-echo                           Room %room_number% >>.\chat.txt
-echo.  >>.\chat.txt
-goto read_messages_room
-
-:quit
-echo %time% - %username% has left the room >>".\chat.txt"
-timeout /t 1 >nul
-TASKKILL /F /IM cmd.exe /T
-goto read_messages_room
 
 :exitChat
 exit
+
+:banCheck
+set "banned="
+for /f "usebackq delims=" %%a in (".\banned_users.txt") do (
+if /i "%username%"=="%%a" (
+set "banned=true"
+)
+)
+if defined banned (
+echo You have been banned from this room.
+exit ) else (
+goto username
+)
+
+:operatorCheck
+set "operator="
+for /f "usebackq delims=" %%a in (".\operators.txt") do (
+if /i "%username%"=="%%a" (
+set "operator=true"
+)
+)
+if defined operator (
+echo You have operator access.
+) else (
+echo You do not have operator access.
+)
+goto read_messages_room
+
+:username
+set /p username=Username:
+call :banCheck
+call :operatorCheck
+echo %username% >>".\userlist.txt"
+goto room1
